ecdbc8890fa74b7dfedd3002ff34ee61f93b2259
Diego <matos.diegom@gmail.com>
2026-09-15 10:42:25 -0300

    feat: perfil create functions and style improve

--- src/main.js
+++ src/main.js
@@ -5,30 +5,52 @@ const user = {
     photo: null
 }
 
+function getInitials(name) {
+    const names = name.trim().split(' ')
+
+    const firstInitial = names[0][0]
+    const lastInitial = names[names.length - 1][0]
+
+    return firstInitial + lastInitial
+}
+
+function createAvatar(user) {
+    if (user.photo) {
+        return `
+            <img
+                src="${user.photo}"
+                alt="Foto de ${user.name}"
+            >
+        `
+    }
+
+    return getInitials(user.name)
+}
+
 document.querySelector('#app').innerHTML = `
-  <div class="perfil-card">
+    <div class="perfil-card">
 
-    <div class="perfil-image">
-      DM
+        <div class="perfil-image">
+            ${createAvatar(user)}
+        </div>
+
+        <h1>${user.name}</h1>
+
+        <p class="perfil-role">Desenvolvedor Backend</p>
+
+        <p class="perfil-description">
+            Estudante de Sistemas de Informação interessado em desenvolvimento
+            backend e tecnologias Java.
+        </p>
+
+        <div class="perfil-info">
+            <span>Ceará, Brasil</span>
+            <span>Java</span>
+        </div>
+
+        <button>
+            <a href='https://www.linkedin.com/in/diegodmatos/' class="perfil-link"> Ver Perfil</a>
+        </button>
+
     </div>
-
-    <h1>Diego Matos</h1>
-
-    <p>Desenvolvedor Backend</p>
-
-    <p>
-      Estudante de Sistemas de Informação interessado em desenvolvimento
-      backend e tecnologias Java.
-    </p>
-
-    <div>
-      <span>Ceará, Brasil</span>
-      <span>Java</span>
-    </div>
-
-    <button>
-      Ver perfil
-    </button>
-
-  </div>
 `
